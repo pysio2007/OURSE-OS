7a250d176c7bc70099cb69c4388006366ecd4928
lhr20070406 <593277393@qq.com>
2021-01-04 21:52:56 +0800

    A1

--- 源码/第一个测试版本.asm
+++ 源码/第一个测试版本.asm
@@ -2,15 +2,17 @@ org 07c00h
 mov ax,cs
 mov ds,ax
 mov es,ax
-call $
-Disp:
-     mov ax,BootMsg
-	 mov bp,ax
-	 mov cx,16
-	 mov ax,01301h
-	 mov bx,000ch
-	 mov dl,0
-	 int 10h
-BootMsg: db "Hello, OURSE OS"
-times 510 - ($-$$) db 0
+call DD
+jmp $
+DD:
+	mov ax,BOOTMSG
+	mov bp,ax
+	mov cx,16
+	mov ax,01301h
+	mov bx,000ch
+	mov dl,0
+	int 10h
+	ret
+BOOTMSG:	db "hello, OS World!"
+times 510-($-$$)	db 0
 dw 0xaa55
